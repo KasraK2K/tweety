90bbc40112ccef53c08f91a569b56306b9c71f45
Kasra <Kasra_K2K@yahoo.com>
2020-11-03 22:05:50 +0330

    create components

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -30,28 +30,14 @@
 		<section class="px-8 py-4 mb-6">
 			<header class="container mx-auto">
 				<h1>
-					<img src="/images/logo.svg" alt="Tweety">
+					<a href="{{ route('home') }}">
+						<img src="/images/logo.svg" alt="Tweety">
+					</a>
 				</h1>
 			</header>
 		</section>
 
-		<section class="px-8">
-			<main class="container mx-auto">
-                <div class="lg:flex lg:justify-between">
-                    <div class="lg:w-32">
-                        @include('_sidebar-links')
-                    </div>
-
-                    <div class="lg:flex-1 lg:mx-10">
-                        @yield('content')
-                    </div>
-
-                    <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4 h-full">
-                        @include('_friends-list')
-                    </div>
-                </div>
-			</main>
-		</section>
+		{{ $slot }}
 	</div>
 </body>
 </html>

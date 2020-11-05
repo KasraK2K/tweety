d700165ef9a371f6a3d6525be62b4ff1cde1ecf0
Kasra <Kasra_K2K@yahoo.com>
2020-11-05 14:57:42 +0330

    show and hide user follow/unfollow and edit if is owner or not

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,7 +1,9 @@
-<form action="/profiles/{{ $user->name }}/follow" method="POST">
-	@csrf
+@unless (auth()->user()->is($user))
+	<form action="/profiles/{{ $user->name }}/follow" method="POST">
+		@csrf
 
-	<button type="submit" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
-		{{ auth()->user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
-	</button>
-</form>
+		<button type="submit" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
+			{{ auth()->user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
+		</button>
+	</form>
+@endunless
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -16,8 +16,10 @@
 				</div>
 
 				<div class="flex">
-					<a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">Edit
-						Profile</a>
+					@if (auth()->user()->is($user))
+						<a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">Edit
+							Profile</a>
+					@endif
 
 					<x-follow-button :user="$user"></x-follow-button>
 				</div>
